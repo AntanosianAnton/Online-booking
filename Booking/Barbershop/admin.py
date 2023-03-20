ad6dad7dc0ae90345f9acc715f70d734c29141da
Anton <anton,antanosyan@gmail.com>
2023-03-21 00:02:07 +0300

    added admin.py

--- Booking/Barbershop/admin.py
+++ Booking/Barbershop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Appointment, Master, Service
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'barber',
+        'customer_name',
+        'customer_phone_number',
+        'service',
+        'date',
+        'time'
+        )
+    
+
+
+admin.site.register(Appointment)
+admin.site.register(Master)
+admin.site.register(Service)
