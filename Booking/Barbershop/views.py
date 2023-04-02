842752bd6a0be09a8b9890e50491fd9efff9c9ce
Anton <anton,antanosyan@gmail.com>
2023-04-02 14:33:45 +0300

    view func for my bratha

--- Booking/Barbershop/views.py
+++ Booking/Barbershop/views.py
@@ -1,13 +1,29 @@
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.utils import timezone
+
 from .models import Barber, Service, Appointment
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+    def appointment_list(request):
+        appointments = Appointment.objects.filter(
+            date__gte=timezone.now() - timezone.timedelta(days=56)
+        ).order_by('-date', 'time')
+        paginator = Paginator(appointments, 28)
+        page = request.GET.get('page')
+        appointments_page = paginator.get_page(page)
+        return render(
+            request,
+            'home.html', {'appointments_page': appointments_page}
+        )
+
 
 class BarberPageView(TemplateView):
-    template_name = 'Barbershop/templates/barbers.html'
+    template_name = 'barbers.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -16,7 +32,7 @@ class BarberPageView(TemplateView):
 
 
 class ServicePageView(TemplateView):
-    template_name = 'Barbershop/services.html'
+    template_name = 'services.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,7 +41,7 @@ class ServicePageView(TemplateView):
 
 
 class AppointmentPageView(TemplateView):
-    template_name = 'Barbershop/appointment.html'
+    template_name = 'appointment.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
